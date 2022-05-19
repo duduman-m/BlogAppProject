6974432faf84905e271196bd809988cdd68f9355
DudumanMarius <marius.duduman@student.usv.ro>
2022-05-19 13:47:30 +0300

    added entrypoint

---
+++ blogapp/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "dbtest" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+
+exec "$@"
